1a225beeb4732a36fa05bab7405c5e1620443af7
Саня <0088664@mail.ru>
2022-11-07 05:17:35 +0300

    tests for first two

--- src/test/java/ru/akirakozov/sd/refactoring/servlet/ServletTestingBase.java
+++ src/test/java/ru/akirakozov/sd/refactoring/servlet/ServletTestingBase.java
@@ -2,25 +2,82 @@ package ru.akirakozov.sd.refactoring.servlet;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public abstract class ServletTestingBase {
-//    why is product db named test? don't want to mess with it so "test_test" it is then
-    private final String dbURL = "jdbc:sqlite:test_test.db";
+    //    why is product db named test?
+    private final String dbURL = "jdbc:sqlite:test.db";
 
     Server server;
     final int PORT = 8081;
 
     abstract void addServlet(ServletContextHandler context);
 
-    @BeforeAll
-    void create() {
+    static String executeURL(URL url) {
+        try {
+            return new BufferedReader(
+                    new InputStreamReader(url.openStream(), StandardCharsets.UTF_8))
+                    .lines()
+                    .collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    void addProduct(Product product) {
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+            String sql = "INSERT INTO PRODUCT " +
+                    "(NAME, PRICE) VALUES (\"" + product.name + "\"," + product.price + ")";
+            Statement stmt = c.createStatement();
+            stmt.executeUpdate(sql);
+            stmt.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    class Product {
+        String name;
+        long price;
+
+        public Product(String name, long price) {
+            this.name = name;
+            this.price = price;
+        }
+    }
+
+    ArrayList<Product> getAllProducts() {
+        ArrayList<Product> allProducts = new ArrayList<>();
+        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db")) {
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT");
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                long price = rs.getLong("price");
+                allProducts.add(new Product(name, price));
+            }
+
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return allProducts;
+    }
+
+    ServletTestingBase() {
         try (Connection c = DriverManager.getConnection(dbURL)) {
             String sql = "CREATE TABLE IF NOT EXISTS PRODUCT" +
                     "(ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
@@ -57,6 +114,15 @@ public abstract class ServletTestingBase {
             stmt.executeUpdate(sql);
             stmt.close();
         } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @AfterAll
+    void stop() {
+        try {
+            server.stop();
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
